b58d7bf8684783f303ec5f7368a8d529338032e1
warlyware <danward@gmail.com>
2016-02-12 20:34:16 -0500

    Add colors on mouse over of top and bottom of .frontpage, add links to fb page and yt video

--- script.js
+++ script.js
@@ -5,5 +5,26 @@ angular.module('DrScience', []);
 angular.module('DrScience')
     .controller('MainController', ['$scope', function($scope) {
 
+    var vm = this;
+
+    var fpLink1 = angular.element(document.getElementById('fp-link-1'));
+    fpLink1.on('mouseenter', function() {
+        fpLink1.addClass('hover-color-1');
+    });
+    fpLink1.on('mouseleave', function() {
+        fpLink1.removeClass('hover-color-1');
+    });
+
+    var fpLink2 = angular.element(document.getElementById('fp-link-2'));
+    fpLink2.on('mouseenter', function() {
+        fpLink2.addClass('hover-color-2');
+    });
+    fpLink2.on('mouseleave', function() {
+        fpLink2.removeClass('hover-color-2');
+    });
+
+
+
+
     console.log('DR.SCIENCE ONLINE');
 }]);
