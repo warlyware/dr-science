4b3152435201759aefe390d3a25557e1743a9c84
warlyware <danward@gmail.com>
2016-02-12 19:10:20 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'strict mode';
+
+angular.module('DrScience', []);
+
+angular.module('DrScience')
+    .controller('MainController', ['$scope', function($scope) {
+
+    console.log('DR.SCIENCE ONLINE');
+}]);
